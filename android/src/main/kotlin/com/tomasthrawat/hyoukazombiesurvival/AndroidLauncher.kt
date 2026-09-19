83b1459de100f0ac2aaeea8a3ffcf3c75e9398c7
TomasThrawat <tomastharwat873@gmail.com>
2026-09-19 17:23:49 +0300

    Fix Android runtime crash configuration

--- android/src/main/kotlin/com/tomasthrawat/hyoukazombiesurvival/AndroidLauncher.kt
+++ android/src/main/kotlin/com/tomasthrawat/hyoukazombiesurvival/AndroidLauncher.kt
@@ -1,12 +1,29 @@
 package com.tomasthrawat.hyoukazombiesurvival
+
 import android.os.Bundle
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.tomasthrawat.hyoukazombiesurvival.game.SurvivalGame
+
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val config = AndroidApplicationConfiguration().apply { useImmersiveMode = true; numSamples = 2; useCompass = false; useAccelerometer = false }
+
+        val config = AndroidApplicationConfiguration().apply {
+            // Use the most conservative Android graphics configuration for
+            // broad GLES2/GLES3 device compatibility.
+            useImmersiveMode = false
+            hideStatusBar = true
+            hideNavBar = true
+            numSamples = 0
+            useAccelerometer = false
+            useCompass = false
+            useGyroscope = false
+            useRotationVectorSensor = false
+            useWakelock = true
+            useGL30 = false
+        }
+
         initialize(SurvivalGame(), config)
     }
 }
